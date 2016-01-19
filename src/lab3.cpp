0ab8172d134f7220a3c7c28a48933e9f09381ab1
curtywang <ycwang@u.northwestern.edu>
2016-01-19 11:48:30 -0600

    Initial commit

---
+++ src/lab3.cpp
@@ -0,0 +1,14 @@
+#include "eecs230.h"
+
+//
+// Lab 3 EXAMPLE functions
+//
+
+/* The main function, or the "entry function" of the lab3 program, simply calls the other
+ * functions to run.
+ */
+int main() {
+
+    return 0; // Lets the operating system know everything ran successfully.
+}
+
