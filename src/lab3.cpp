e51c580829759c8d64b91c750b75690eed5ae02c
sinantuna <sinantuna2018@u.northwestern.edu>
2016-01-20 14:57:47 -0600

    lab3

--- src/lab3.cpp
+++ src/lab3.cpp
@@ -7,7 +7,41 @@
 /* The main function, or the "entry function" of the lab3 program, simply calls the other
  * functions to run.
  */
+
+double compute_cm(double input_inches){
+    double output_cm=input_inches*2.54;
+    return output_cm;
+
+}
+
+void print_results(vector<double> inputs_inches){
+    double input_inches;
+    double output_cm;
+
+    for (int i=0; i<inputs_inches.size(); i++){
+        input_inches=inputs_inches[i];
+        output_cm = compute_cm(input_inches);
+        cout<<input_inches<<" in = "<<output_cm<<" cm\n";
+
+    }
+}
+
+void prompt_for_inches(){
+    vector<double> inputs_inches;
+    double input_inches;
+
+    cout<<"Enter lengths in inches (stop with 0): \n";
+
+    while (cin >> input_inches && input_inches != 0){
+        inputs_inches.push_back(input_inches);
+    }
+
+    print_results(inputs_inches);
+}
+
 int main() {
+    prompt_for_inches();
+
 
     return 0; // Lets the operating system know everything ran successfully.
 }
